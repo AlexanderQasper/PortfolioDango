4cc07b2f2b9a88d11e164e4ca41db9211bbc9fbd
AlexQasper <semidog@gmail.com>
2025-05-09 17:22:04 +0400

    anti DDOS sysetm Done

--- core/settings.py
+++ core/settings.py
@@ -294,6 +294,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '10/minute',
+        'anon': '5/minute',
+    },
 }
 
 # DRF Spectacular settings
